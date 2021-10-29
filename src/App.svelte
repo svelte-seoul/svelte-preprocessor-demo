4326d2e6732ec2e86c5b53d3e1c6cbcef18b5436
yujong-lee <yujonglee@g.skku.edu>
2021-10-30 00:55:32 +0900

    Initialize with style props: --my-color
    https://svelte.dev/docs#style_props

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Welcome from './Welcome.svelte'
+</script>
+
+<main>	
+	<h1>Demo</h1>
+	<Welcome name='Jayden' --my-color='blue'/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 480px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: purple;
+	}
+</style>
---
+++ src/Welcome.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name = '?';
+</script>
+
+<h2>Hello {name}!</h2>
+<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+<style>
+	h2 {
+		color: var(--my-color);
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
